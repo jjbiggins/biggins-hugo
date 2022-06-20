6e1ebcf7d579ca7d1fbf9778a0cc696aa34e0264
Joe Biggins <jjbiggins+github@biggins.tech>
2022-06-20 03:07:29 -0500

    deleted unused assets dir

--- assets/js/toggleDarkTheme.js
+++
@@ -1,9 +0,0 @@
-var toggle = document.getElementById("dark-mode-toggle");
-
-toggle.addEventListener("click", () => {
-    if (document.documentElement.classList.contains('dark-theme')) {
-        setTheme("light");
-    } else {
-        setTheme("dark");
-    }
-});
